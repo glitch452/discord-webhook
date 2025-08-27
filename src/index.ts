cb5628c6d5f69846c26655909d905d0eb32fb4a4
glitch452 <894128+glitch452@users.noreply.github.com>
2025-08-27 09:48:41 -0700

    feat: Re-export all types from main barrel file

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,25 @@ const discordWebhookNode = {
 export default discordWebhookNode;
 
 export { Webhook, MessageBuilder, PollBuilder, EmbedBuilder };
+
+export * from './types/ActionRow.js';
+export * from './types/AllowedMentions.js';
+export * from './types/Attachment.js';
+export * from './types/BuildTuples.js';
+export * from './types/Button.js';
+export * from './types/Container.js';
+export * from './types/Embed.js';
+export * from './types/Emoji.js';
+export * from './types/enums.js';
+export * from './types/File.js';
+export * from './types/MediaGallery.js';
+export * from './types/MessageComponent.js';
+export * from './types/PollCreateRequest.js';
+export * from './types/Section.js';
+export * from './types/Select.js';
+export * from './types/Separator.js';
+export * from './types/TextDisplay.js';
+export * from './types/TextInput.js';
+export * from './types/Thumbnail.js';
+export * from './types/UnfurledMediaItem.js';
+export * from './types/WebhookPayload.js';
